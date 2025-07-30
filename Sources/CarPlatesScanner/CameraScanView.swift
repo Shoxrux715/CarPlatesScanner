6e9fa68f2a24e42bc48c7a68a35b190e01450f13
Shoxrux715 <109664923+Shoxrux715@users.noreply.github.com>
2025-07-30 19:39:54 +0500

    Plates validation updated

--- Sources/CarPlatesScanner/CameraScanView.swift
+++ Sources/CarPlatesScanner/CameraScanView.swift
@@ -164,10 +164,13 @@ public extension String {
     func isValidNumberPlates() -> Bool {
 //        #"^[0-9][0-9O](?:[A-Z][0-9]{3}[A-Z]{2}|[0-9]{3}[A-Z]{3}| ?[A-Z] ?[0-9]{5,6})$"#
         let patt = #"^(?:[0-9][0-9O](?:[A-Z][0-9]{3}[A-Z]{2}|[0-9]{3}[A-Z]{3}| ?[A-Z] ?[0-9]{5,6})|[A-Z]{2}[0-9]{4}|[A-Z]{3}[0-9]{3})$"#
-        
-        let regex = try! NSRegularExpression(pattern: patt)
-        let range = NSRange(location: 0, length: self.utf16.count)
-        return regex.firstMatch(in: self, options: [], range: range) != nil
-        
+        do { 
+            let regex = try NSRegularExpression(pattern: patt)
+            let range = NSRange(location: 0, length: self.utf16.count)
+            return regex.firstMatch(in: self, options: [], range: range) != nil
+        } catch { 
+            print("Regex error: ", error)
+            return false
+        }
     }
 }
