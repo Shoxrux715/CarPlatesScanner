adcd28926396d12ddc2b4dc7f580e40b0b201ca7
Shoxrux715 <shokh715@mail.ru>
2025-08-11 16:01:19 +0500

    fixes

--- Sources/CarPlatesScanner/CameraScanView.swift
+++ Sources/CarPlatesScanner/CameraScanView.swift
@@ -63,43 +63,44 @@ public struct CameraScanView: View {
         NavigationStack {
             ZStack {
                 if accessGranted {
-                    CarPlatesScannerView { plates in
-                        self.carPlates = plates
-                        parseCarPlate(plates)
-                        cameraAutoOff()
-                    }
-                    .edgesIgnoringSafeArea(.all)
-                    
-                    Rectangle()
-                        .fill(cameraViewBgColor.opacity(cameraViewBgColorOpacity))
-                        .mask(
-                            CutoutMask(size: cutoutSize)
-                                .fill(style: FillStyle(eoFill: true))
-                        )
-                        .background(
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(cutoutStrokeColor, lineWidth: cutoutStrokeLineWidth)
-                                .frame(
-                                    width: cutoutWidth,
-                                    height: cutoutHeight)
-                        )
-                        .overlay {
-                            HStack(alignment: .center) {
-                                if !carPlates.isEmpty {
-                                    Text("\(carPlates)")
-                                        .font(font)
-                                        .foregroundStyle(scannedPlatesTextColor)
-                                }
-                            }
-                            .animation(.easeInOut, value: carPlates)
-                        }
-                        .ignoresSafeArea()
-                    
-                    
-                    Text("point-the-camera", bundle: .module)
-                        .font(font)
-                        .foregroundColor(Color.white)
-                        .frame(maxHeight: 300, alignment: .top)
+//                    CarPlatesScannerView { plates in
+//                        self.carPlates = plates
+//                        parseCarPlate(plates)
+//                        cameraAutoOff()
+//                    }
+//                    .edgesIgnoringSafeArea(.all)
+//                    
+//                    Rectangle()
+//                        .fill(cameraViewBgColor.opacity(cameraViewBgColorOpacity))
+//                        .mask(
+//                            CutoutMask(size: cutoutSize)
+//                                .fill(style: FillStyle(eoFill: true))
+//                        )
+//                        .background(
+//                            RoundedRectangle(cornerRadius: 16)
+//                                .stroke(cutoutStrokeColor, lineWidth: cutoutStrokeLineWidth)
+//                                .frame(
+//                                    width: cutoutWidth,
+//                                    height: cutoutHeight)
+//                        )
+//                        .overlay {
+//                            HStack(alignment: .center) {
+//                                if !carPlates.isEmpty {
+//                                    Text("\(carPlates)")
+//                                        .font(font)
+//                                        .foregroundStyle(scannedPlatesTextColor)
+//                                }
+//                            }
+//                            .animation(.easeInOut, value: carPlates)
+//                        }
+//                        .ignoresSafeArea()
+//                    
+//                    
+//                    Text("point-the-camera", bundle: .module)
+//                        .font(font)
+//                        .foregroundColor(Color.white)
+//                        .frame(maxHeight: 300, alignment: .top)
+                    Text("тут должна быть камера")
                 } else {
                     VStack {
                         Text("no-access", bundle: .module)
